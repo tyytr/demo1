08a89398cfe4100c7e0ec8194994c89fa0ec6d0a
root <gyp0925vip@outlook.com>
2018-02-08 16:31:03 +0800

    添加图片上传接口

---
+++ src/main/java/com/change/demo002/controller/ImageController.java
@@ -0,0 +1,10 @@
+package com.change.demo002.controller;
+
+/**
+ * @Author: Gaoyp
+ * @Description:
+ * @Date: Created in 下午3:55 18-2-8
+ * @Modified By:
+ */
+public class ImageController {
+}
---
+++ src/main/java/com/change/demo002/utils/FileUtils.java
@@ -0,0 +1,10 @@
+package com.change.demo002.utils;
+
+/**
+ * @Author: Gaoyp
+ * @Description:
+ * @Date: Created in 下午4:03 18-2-8
+ * @Modified By:
+ */
+public class FileUtils {
+}
